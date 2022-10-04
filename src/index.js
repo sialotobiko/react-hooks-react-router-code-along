af90d6d379ee2bacf51111c8686443a116c82534
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-04 23:10:47 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import App from "./App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>, 
+document.getElementById("root"));
